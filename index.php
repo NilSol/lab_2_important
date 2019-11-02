d5eb04d997892844524f7c862ca859d4045ea89a
NikSol <nikisoloshenko@gmail.com>
2019-11-02 17:37:37 +0200

    Add my files

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-if(session_status()!=PHP_SESSION_ACTIVE) session_start();
+ session_start();
+session_destroy();
+session_unset();
+ /* После того как пользователь ввел свои данные его перенаправляют на этот файл если данные верные,
+  то идет приветствие если нет то файл перенаправляет на language.php*/
 ?>
 <!doctype html>
 <html lang="en">
@@ -15,62 +19,50 @@ if(session_status()!=PHP_SESSION_ACTIVE) session_start();
 </head>
 <body>
 <?php
-$users = [
-    ['login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'],
-    ['login' => 'Afanasiy', 'password' => '54321', 'lang' => 'en'],
-    ['login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'],
-    ['login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'],
-    ['login' => 'Sasha', 'password' => 'Ignorantia_non_excusat '],
-];
-$tongue= [
-        'ru' => ['Здравствуйте', 'Вы на главной странице' , 'Инструкия к использованию' ,'Не трогай меня и я не трону тебя'],
-        'en' => ['Hello', 'You are on the main page', 'Instructions for use', 'Do not touch me and I will not touch you'],
-        'ua' => ['Вітаємо','Ви на головній сторінці','Інструкція до використання','Чи не чіпай мене і я не трону тебе'],
-        'it' =>['Ciao','Sei nella pagina principale','Istruzioni per l`uso','Non toccarmi e io non ti toccherò'],
-]
+$users = array (
+  array  ('login' => 'Vasisualiy', 'password' => '12345', 'lang' => 'ru'),
+   array ('login' => 'Afanasiy', 'password' => '54321', 'lang' => 'en'),
+   array ('login' => 'Petro', 'password' => 'EkUC42nzmu', 'lang' => 'ua'),
+   array ('login' => 'Pedrolus', 'password' => 'Cogito_ergo_sum', 'lang' => 'it'),
+   array ('login' => 'Sasha', 'password' => 'Ignorantia_non_excusat '),
+);
+$tongue= array(
+        'ru' => array ('Здравствуйте', 'Вы на главной странице' ,'Не трогай меня и я не трону тебя'),
+        'en' => array ('Hello', 'You are on the main page',  'Do not touch me and I will not touch you'),
+        'ua' => array ('Вітаємо','Ви на головній сторінці', 'Чи не чіпай мене і я не трону тебе'),
+        'it' =>array('Ciao','Sei nella pagina principale','Non toccarmi e io non ti toccherò'),
+)
 ?>
-    <form method="post" action="" align = 'middle'>
-        <div class="form-wrap">
-            <div class="profile">
-                <h1>Вход</h1>
-            </div>
-        <div>
-            <label for="login">login</label>
-            <input type="text" name="login" required>
-        </div>
-        <div>
-            <label for="password">password</label>
-            <input type="password" name="password" required>
-        </div>
-        </div>
-        <div>
-            <select name="language">
-                <option>Выберите язык</option>
-                <option value="ru">Русский</option>
-                <option value="en">English</option>
-                <option value="ua">Український</option>
-                <option value="it">Italiano</option>
-            </select>
-            <div class="select-arrow"></div>
-        <button type="submit" class="btn btn-outline-primary">Отправить</button>
-        </div>
-    </form>
+
+
 <?php
-if($_SESSION['login'] == 'Vasisualiy' AND $_SESSION['password'] = '12345')
-{
-    echo 'Z ybrbnfn';
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['password'] = $_POST['password'];
+ 
+foreach ($users as $key=>$value) {
+    if ($value['login'] == $_POST['login'] && $value['password'] == $_POST['password'])
+    {
+        $_SESSION['login'] = $_POST['login'];
+        $_SESSION['lang'] = $value['lang'];
+        $_SESSION['user_id']=$key;
+        break;
+    }
+    else
+    {
+        include 'call_forwarding.php';
+    break;
+    }
 }
+$forename = $_SESSION['login'];
+  $lang = $users[ $_SESSION['user_id']]['lang'];
 ?>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4"><?php echo $forename . ' '. $tongue[$lang][0]; ?></h1
+    </div>
+</div>
+<script type="text/javascript"> setTimeout(function(){
+        window.location.href = 'google.com';
+    }, 3 * 1000); </script>
 </body>
 </html>
